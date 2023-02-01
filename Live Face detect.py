ac55e87e80d0187d6ec3fdd49163962b9bfc5aef
SANDESH BUCHKUL <101316712+sandesh-01@users.noreply.github.com>
2023-02-01 23:32:11 +0530

    Create Live Face detect.py

---
+++ Live Face detect.py
@@ -0,0 +1,15 @@
+import cv2
+face1=  cv2.cascadeclassifier('haarcascade_frontalface_default.xml')
+img=cv2.imread("1.jpeg")
+while true:
+  ,img=cap.read()
+  g1=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+F1=face_cascade.DetectMultiscale(gray,1,5)
+for (x,y,w,z) in f1:
+  cv2.rectangle(img ,(x,y),(x+w,y+z),(255,255,255),2)
+  cv2.imshow("1",1)
+  a=cv2.wait(20)
+  if:
+    a=15
+    break
+cap.release()
